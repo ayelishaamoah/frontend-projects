7a2c75a5f1777896765289a1f61ffbe19ce6c732
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2021-05-10 19:32:08 +0100

    style: Fix font sizes

--- src/components/Typography/Typography.styles.tsx
+++ src/components/Typography/Typography.styles.tsx
@@ -9,22 +9,26 @@ type TypographyProps = {
 export const H2 = styled.h2`
     color: ${theme.colours.primaryOne};
     font-family: ${theme.fontFamily};
-    font-size: 1.5em;
+    font-size: 1.5rem;
+    font-weight: ${theme.fontWeights.normal};    
     margin-top: 0;
+    letter-spacing: 1px;
 `
 
 export const P = styled.p`
     color: ${(props:TypographyProps) => props.fontColour};
     font-family: ${theme.fontFamily};
-    font-size: 1em;
-    line-height: 0.9em;
+    font-size: 0.9rem;
+    line-height: 1rem;
     margin-bottom: 9px;
     margin-top: 0;
+    letter-spacing: 0.5px;
+    font-weight: 300;
 `
 
 
 export const Caption = styled(P)`
-    font-size: 0.75em;
+    font-size: 0.75rem;
 `
 
 export const H3 = styled.h3`
@@ -32,9 +36,13 @@ export const H3 = styled.h3`
     font-family: ${theme.fontFamily};
     font-weight: ${theme.fontWeights.normal};
     margin-top: 0;
+    letter-spacing: 1px;
 `
 
 export const Overline = styled.span`
-    font-size: 0.45em;
+    font-size: 0.8rem;
     font-weight: ${theme.fontWeights.light};
+    vertical-align: bottom;
+    filter: opacity(0.6);
+    margin-left: 4px;
 `
